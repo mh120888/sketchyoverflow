9d4fb08f3a8c1135800a43dd58cc0834ada2e9ca
Spencer Smitherman & John Olmsted <pair+bigspencey.qsymmachus@devbootcamp.com>
2014-02-20 16:04:44 -0800

    Adds initial rails skeleton

---
+++ sketchy-overflow/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SketchyOverflow::Application.config.secret_token = '711ccba883ea72f13dee06027de6acf84ad9f65c651e36fd3f9a77ab6769dfca1401731999f1134650719387e1fcaf32a5369d8f6ee7a357b8db9bd7ff3c6b66'
