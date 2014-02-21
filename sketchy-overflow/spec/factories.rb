ce0e5a06f5a76bc353da0af4a82559bdd77d90b8
John Olmsted <apprentice@dbc23.local>
2014-02-20 17:08:53 -0800

    Questions now have titles

--- sketchy-overflow/spec/factories.rb
+++ sketchy-overflow/spec/factories.rb
@@ -1,9 +1,9 @@
 FactoryGirl.define do
   factory :question do
-    question { Faker::Lorem.sentence }
+    content { Faker::Lorem.sentence }
   end
 
   factory :answer do
-    question { Faker::Lorem.sentence }
+    content { Faker::Lorem.sentence }
   end
 end
