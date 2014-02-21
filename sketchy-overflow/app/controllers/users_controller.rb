94033f89a91991099c5b9730309821b2a709d9cd
Eli Shkurkin <eshkurkin@gmail.com>
2014-02-21 12:31:01 -0800

    Add create a user functionality
    validations not yet included

--- sketchy-overflow/app/controllers/users_controller.rb
+++ sketchy-overflow/app/controllers/users_controller.rb
@@ -8,6 +8,11 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(params[:user])
+    if @user.save
+      redirect_to user_path(@user)
+    else
 
+    end
   end
 end
