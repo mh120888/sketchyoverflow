d8a334938610fbcd764e5a4ad67f50d0354f715c
mh120888 <matthew.alan.higgins@gmail.com>
2014-02-20 22:05:06 -0800

    Adds additional styling to new question form implements questions_controller tests for #new implements feature tess for navigating from questions#index to questions#new implements feature test for posting a new question and viewing it on home page

--- sketchy-overflow/spec/features/questions_spec.rb
+++ sketchy-overflow/spec/features/questions_spec.rb
@@ -7,5 +7,21 @@ describe "Questions" do
       visit root_path
       expect(page).to have_content(question.content)
     end
+    it 'allows a user to navigate to new question page' do
+      visit root_path
+      click_on('Ask a Question')
+      expect(current_path) == new_question_path
+    end
+  end
+  context 'New question' do
+    it 'will appear on the home page when posted' do
+      visit new_question_path
+      fill_in('Title', :with => 'Dry skin') 
+      fill_in('Content', :with => 'I have dry skin. What do I do?')
+      click_on('Create Question')
+      expect(current_path) == questions_path
+      visit root_path
+      expect(page).to have_content('Dry skin')
+    end
   end
 end
