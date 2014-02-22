f08c81926303f5cb204b0a0cf3764386d0ff5a85
John Olmsted <apprentice@dbc23.local>
2014-02-21 16:52:04 -0800

    Create answer form is now working

--- sketchy-overflow/app/assets/javascripts/answer.js
+++ sketchy-overflow/app/assets/javascripts/answer.js
@@ -2,13 +2,15 @@ var Answer = {
   bindEvents: function () {
     $('form#new_answer').on('ajax:success', this.appendAnswer)
     $('form#new_answer').on('ajax:error', this.appendError)
-  }
+  },
 
   appendAnswer: function(event, data, status, xhr) {
-    $('.answer_list').prepend(data)
+    event.preventDefault()
+    $('.answers_list').prepend(data)
   },
 
   appendError: function(event, xhr, status, error) {
+    event.preventDefault()
     $('#answer_form').append(xhr.responseText)
   }
 }
