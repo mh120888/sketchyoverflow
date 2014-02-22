06c0481dd08e30bb1cbcaa2536f74cb4d3008739
Ricardo Rubio <rickarubio@gmail.com>
2014-02-21 16:26:48 -0800

    Updates migration and schema to use text datatype instead of string to accomodate NSA-proof encryption scheme on user credentials

--- sketchy-overflow/db/migrate/20140221010104_create_users.rb
+++ sketchy-overflow/db/migrate/20140221010104_create_users.rb
@@ -1,8 +1,8 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :name
-      t.string :password
+      t.text :name
+      t.text :password
 
       t.timestamps
     end
