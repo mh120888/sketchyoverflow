ceb3452c5309e244b94214ade467fd818952dd30
Ricardo Rubio <rickarubio@gmail.com>
2014-02-21 16:27:59 -0800

    Adds unit tests for #encrypt_credentials and updates other authentication related tests to be compatible with newly implemented encryption scheme.

--- sketchy-overflow/spec/models/user_spec.rb
+++ sketchy-overflow/spec/models/user_spec.rb
@@ -5,7 +5,7 @@ describe User do
   context "#authenticate" do
     it "should find existing user" do
       user = User.create(name: "foo", password: "bar")
-      expect(User.authenticate({username: user.name, password: user.password}).class).to be User
+      expect(User.authenticate({username: "foo", password: "bar"}).class).to be User
     end
 
     it "should be false if not found" do
@@ -13,9 +13,21 @@ describe User do
     end
   end
 
-  context "encryption" do
-    it "should do stuff" do
-      pending
+  describe "#encrypt_credentials!" do
+    context "before saving to the database" do
+      it "encrypts the user's password using SCrypt" do
+        user = User.create(name: "bob", password: "test1234")
+        user.reload
+        expect(user.password).not_to eq("test1234")
+        expect(SCrypt::Password.new(user.password)).to be_an_instance_of SCrypt::Password
+      end
+      it "hashes the user's username using SHA512" do
+        user = User.create(name: "roger", password: "test1234")
+        user.reload
+        expect(user.name).not_to eq("roger")
+        expect(user.name.length).to eq(128)
+      end
     end
   end
+
 end
